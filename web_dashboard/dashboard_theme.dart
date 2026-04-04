81086086fc906581280c0eddf20ee7de86c578e6
Ulisses <Ulissesxavier37@gmail.com>
2026-04-04 12:37:11 -0400

    Delete web_dashboard directory

--- web_dashboard/dashboard_theme.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-class DashboardTheme {
-  static final Color background = Color.fromARGB(255, 8, 8, 8);
-  static final Color primary = Color.fromARGB(255, 16, 16, 16);
-  static final Color secondary = Color.fromARGB(255, 18, 18, 18);
-  static final Color blue = Color.fromARGB(255, 56, 92, 182);
-  static final Color green = Color.fromARGB(255, 20, 213, 107);
-}
